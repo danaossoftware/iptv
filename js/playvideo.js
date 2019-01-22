341183d132ecf92bf62db3ac6d25b7cae987426c
Dana Prakoso <danaossoftware@gmail.com>
2019-01-22 17:45:11 +0700

    commit

--- js/playvideo.js
+++ js/playvideo.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    var params = location.search;
+    /*var params = location.search;
     params = params.substr(1, params.length);
     var videoURL = params.split("&")[0].split("=")[1];
     videoURL = decodeURIComponent(videoURL);
@@ -15,13 +15,7 @@ $(document).ready(function() {
             $("#loading-container").css("display", "none");
             //video.play();
         });
-    }
-    // hls.js is not supported on platforms that do not have Media Source Extensions (MSE) enabled.
-    // When the browser has built-in HLS support (check using `canPlayType`), we can provide an HLS manifest (i.e. .m3u8 URL) directly to the video element throught the `src` property.
-    // This is using the built-in support of the plain video element, without using hls.js.
-    // Note: it would be more normal to wait on the 'canplay' event below however on Safari (where you are most likely to find built-in HLS support) the video.src URL must be on the user-driven
-    // white-list before a 'canplay' event will be emitted; the last video event that can be reliably listened-for when the URL is not on the white-list is 'loadedmetadata'.
-    else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+    } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
         video.src = videoURL;
         video.addEventListener('loadedmetadata',function() {
             video.play();
@@ -35,7 +29,7 @@ $(document).ready(function() {
             $("#titlebar").hide();
             $("#titlebar").fadeIn(300);
         }
-    });
+    });*/
 });
 
 function pauseVideo() {
@@ -46,6 +40,6 @@ function pauseVideo() {
 }
 
 function playVideo() {
-    $("#play-video-container").fadeOut(300);
+    //$("#play-video-container").fadeOut(300);
     $("#video")[0].play();
 }
