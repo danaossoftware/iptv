821ed6840bd2554e905266480bcf8690ffe44d83
Dana Prakoso <danaossoftware@gmail.com>
2019-01-23 17:11:47 +0700

    commit

--- js/live.js
+++ js/live.js
@@ -240,7 +240,7 @@ function startRecording() {
     recording = true;
     recordingData = [];
     setTimeout(function() {
-        Native.show("Berhenti merekam, file size: "+Object.keys(recordingData).length);
+        Native.show("Berhenti merekam, ukuran file: "+Object.keys(recordingData).length);
         recording = false;
         Native.writeFile("/sdcard/a.mp4", recordingData.toString());
     }, 5000);
