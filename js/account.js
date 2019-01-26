ed9744dd4e6ff20ea9b5ed5d16a7fead6c394f74
Dana Prakoso <danaossoftware@gmail.com>
2019-01-27 05:46:51 +0700

    commit

--- js/account.js
+++ js/account.js
@@ -20,9 +20,16 @@ function loadSettings() {
             $("#username").html(account["username"]);
             var endDate = new Date(parseInt(account["end_date"]));
             $("#end-date").html(endDate.getDate()+" "+getMonthNames(endDate.getMonth())+" "+endDate.getFullYear());
-            if (account["is_trial"] == "true") {
-
+            if (account["is_trial"] == "1") {
+                $("#is-trial").html("YA");
+            } else {
+                $("#is-trial").html("TIDAK");
             }
+            $("#active-connections").html(account["active_connections"]);
+            var madeDate = new Date(parseInt(account["made_on"]));
+            $("#made-on").html(madeDate.getDate()+" "+getMonthNames()[madeDate.getMonth()]+" "+madeDate.getFullYear());
+            $("#maximum-connections").html(account["maximum_connections"]);
+            $("#loading-container").fadeOut(300);
         }
     });
 }
