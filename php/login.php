a2af5cd2ecdf1d501f8a7d856c0fd5fff6fca14c
Dana Prakoso <danaossoftware@gmail.com>
2019-01-27 09:35:33 +0700

    commit

--- php/login.php
+++ php/login.php
@@ -37,7 +37,7 @@ if ($results && $results->num_rows > 0) {
         $activeConnections .= (";" . $ip . ";" . round(microtime(true) * 1000));
     }
     echo $activeConnections . "<br/>";
-    $c->query("UPDATE users SET active_connections=" . $activeConnections . " WHERE id='" . $row["id"] . "'");
+    $c->query("UPDATE users SET active_connections='" . $activeConnections . "' WHERE id='" . $row["id"] . "'");
     session_start();
     $_SESSION["iptvjoss_user_id"] = $row["id"];
     if ($rememberMe == 1) {
