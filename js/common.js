57d8f6851adad643d30a86a3cf2a52665b58170b
Dana Prakoso <danaossoftware@gmail.com>
2019-01-27 12:32:27 +0700

    commit

--- js/common.js
+++ js/common.js
@@ -23,23 +23,22 @@ function loadSettings() {
             var parser = new DOMParser();
             var xml = parser.parseFromString(a, "text/xml");
             xmlData = xml;
-            var common = xml.getElementsByTagName("common")[0];
-            var autoStart = parseInt(common.getElementsByTagName("autostart")[0].childNodes[0].nodeValue);
-            var fullEPG = parseInt(common.getElementsByTagName("full-epg")[0].childNodes[0].nodeValue);
-            var subtitle = parseInt(common.getElementsByTagName("active-subtitle")[0].childNodes[0].nodeValue);
             var userAgent = Native.getUserAgent();
             $("#user-agent").val(userAgent);
-            var language = parseInt(common.getElementsByTagName("language")[0].childNodes[0].nodeValue);
             selectedLanguage = language;
+            var autoStart = Native.readInt("autostart", 0);
             if (autoStart == 1) {
                 $("#auto-start").prop("checked", true);
             }
+            var fullEPG = Native.readInt("full_epg", 0);
             if (fullEPG == 1) {
                 $("#full-epg").prop("checked", true);
             }
+            var subtitle = Native.readInt("active_subtitle", 0);
             if (subtitle == 1) {
                 $("#active-subtitle").prop("checked", true);
             }
+            var language = Native.readInt("language", 0);
             if (language == 0) {
                 $("#language-text").html("Bahasa Indonesia");
             } else if (language == 1) {
@@ -54,7 +53,6 @@ function saveSettings() {
     $("#loading-container").css("display", "flex");
     $("#loading-container").hide();
     $("#loading-container").fadeIn(300);
-    var common = xmlData.getElementsByTagName("common")[0];
     if ($("#auto-start").prop("checked") == true) {
         common.getElementsByTagName("autostart")[0].childNodes[0].nodeValue = 1;
         Native.setStartAtBoot(true);
@@ -63,36 +61,20 @@ function saveSettings() {
         Native.setStartAtBoot(false);
     }
     if ($("#full-epg").prop("checked") == true) {
-        common.getElementsByTagName("full-epg")[0].childNodes[0].nodeValue = 1;
+        Native.writeInt("full_epg", 1);
     } else {
-        common.getElementsByTagName("full-epg")[0].childNodes[0].nodeValue = 0;
+        Native.writeInt("full_epg", 0);
     }
     if ($("#active-subtitle").prop("checked") == true) {
-        common.getElementsByTagName("active-subtitle")[0].childNodes[0].nodeValue = 1;
+        Native.writeInt("active_subtitle", 1);
         Native.enableSubtitle(true);
     } else {
-        common.getElementsByTagName("active-subtitle")[0].childNodes[0].nodeValue = 0;
+        Native.writeInt("active_subtitle", 0);
         Native.enableSubtitle(false);
     }
     var userAgent = $("#user-agent").val();
-    common.getElementsByTagName("user-agent")[0].childNodes[0].nodeValue = userAgent;
-    common.getElementsByTagName("language")[0].childNodes[0].nodeValue = selectedLanguage;
     Native.setUserAgent(userAgent);
-    var serializer = new XMLSerializer();
-    var settingsXML = serializer.serializeToString(xmlData);
-    var fd = new FormData();
-    fd.append("settings", settingsXML);
-    $.ajax({
-        type: 'POST',
-        url: SERVER_URL + 'update-settings.php',
-        data: fd,
-        processData: false,
-        contentType: false,
-        cache: false,
-        success: function (a) {
-            $("#loading-container").fadeOut(300);
-        }
-    });
+    $("#loading-container").fadeOut(300);
 }
 
 function selectLanguage(language) {
