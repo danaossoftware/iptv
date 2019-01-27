547937dafc434f9cb97ec608c560b6fc868e4801
Dana Prakoso <danaossoftware@gmail.com>
2019-01-27 16:44:09 +0700

    commit

--- js/common.js
+++ js/common.js
@@ -56,7 +56,7 @@ function saveSettings() {
     if ($("#auto-start").prop("checked") == true) {
         //Native.setStartAtBoot(1);
     } else {
-        Native.setStartAtBoot(0);
+        //Native.setStartAtBoot(0);
     }
     /*if ($("#full-epg").prop("checked") == true) {
         Native.writeInt("full_epg", 1);
