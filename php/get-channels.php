7be21b43f3f3b5b104fcc2bd98243e0cb189c79c
Dana Prakoso <danaossoftware@gmail.com>
2019-02-21 12:21:59 +0700

    commit

--- php/get-channels.php
+++ php/get-channels.php
@@ -1,6 +1,6 @@
 <?php
-include 'db.php';
+/*include 'db.php';
 $ip = $_SERVER["REMOTE_ADDR"];
 $userId = $c->query("SELECT * FROM sessions WHERE ip='" . $ip . "'")->fetch_assoc()["user_id"];
-$c->query("UPDATE users SET last_update=" . round(microtime(true)*1000) . " WHERE id='" . $userId . "'");
+$c->query("UPDATE users SET last_update=" . round(microtime(true)*1000) . " WHERE id='" . $userId . "'");*/
 echo file_get_contents("../channels.m3u");
