953e3a3e9df907bc0f51c32cb9414b17697b7faa
Dana Prakoso <danaossoftware@gmail.com>
2019-02-24 19:16:29 +0700

    commit

--- php/get-account.php
+++ php/get-account.php
@@ -1,7 +1,10 @@
 <?php
 include 'db.php';
 /*$ip = $_SERVER["REMOTE_ADDR"];
-$userId = $c->query("SELECT * FROM sessions WHERE ip='" . $ip . "'")->fetch_assoc()["user_id"];
-$results = $c->query("SELECT * FROM users WHERE id='" . $userId . "'");*/
+$userId = $c->query("SELECT * FROM sessions WHERE ip='" . $ip . "'")->fetch_assoc()["user_id"];*/
+session_id("jossstream");
+session_start();
+$userId = $_SESSION["jossstream_user_id"];
 $c->query("UPDATE users SET last_update=" . round(microtime(true)*1000) . " WHERE id='" . $userId . "'");
+$results = $c->query("SELECT * FROM users WHERE id='" . $userId . "'");
 echo json_encode($results->fetch_assoc());
