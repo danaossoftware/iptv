11f539b9b07d8004cef9ceae5c1c66e136081a93
Dana Prakoso <danaossoftware@gmail.com>
2019-02-24 18:49:22 +0700

    commit

--- js/notifications.js
+++ js/notifications.js
@@ -1,6 +1,19 @@
+var currentIndex = -1;
 var notificationsJSON;
 
 $(document).ready(function() {
+    if (getLanguage() == 1) {
+        $("#text1").html("BACK");
+        $("#text2").html("NOTIFIKASI");
+        $("#text3").html("BACA NOTIFIKASI");
+        $("#time").html(getTime());
+        $("#date").html(getDate());
+        setTimeout(function() {
+            $("#time").html(getTime());
+            $("#date").html(getDate());
+            setTimeout(this, 1000);
+        }, 1000);
+    }
     getNotifications();
 });
 
@@ -13,6 +26,7 @@ function getNotifications() {
         success: function(a) {
             if (a < 0) {
                 // Error
+                $("#loading-container").fadeOut(300);
             } else {
                 var notifications = JSON.parse(a);
                 notificationsJSON = notifications;
@@ -25,8 +39,12 @@ function getNotifications() {
                         content = content.substr(0, 128);
                         content += "...";
                     }
+                    var date = new Date(parseInt(notification["date"]));
+                    var day = date.getDate();
+                    var month = date.getMonth();
+                    var year = date.getFullYear();
                     items += "<div class=\"notification-item\" style=\"background-color: rgba(136, 136, 136, .5); padding-left: 30px; padding-right: 20px; padding-top: 20px; padding-bottom: 20px; width: calc(100% - 50px); display: flex; flex-flow: row nowrap; margin-top: 10px; overflow: hidden;\">\n" +
-                        "                    <div style=\"color: #eeeeee;\">19:32</div>\n" +
+                        "                    <div style=\"color: #eeeeee;\">"+day+" "+getMonthName(month)+" "+year+"</div>\n" +
                         "                    <div style=\"display: flex; flex-flow: column nowrap; margin-left: 20px;\">\n" +
                         "                        <div style=\"color: white; font-family: 'PalanquinBold'; font-size: 17px;\">"+title+"</div>\n" +
                         "                        <div style=\"color: white; margin-top: 10px;\">"+content+"</div>\n" +
@@ -59,4 +77,41 @@ function openNotificationReviewDialog() {
 
 function closeNotificationReviewDialog() {
     $("#notification-dialog-ctr").fadeOut(300);
+}
+
+function downKey() {
+    currentIndex++;
+    focusToCurrentIndex();
+}
+
+function upKey() {
+    if (currentIndex > 0) {
+        currentIndex--;
+    }
+    focusToCurrentIndex();
+}
+
+function enterKey() {
+    if (currentIndex == 0) {
+        window.history.back();
+    } else {
+        var index = currentIndex-1;
+        var notification = notificationsJSON[index];
+        var title = notification["title"];
+        var content = notification["content"];
+        $("#notification-title").html(title);
+        $("#notification-text").html(content);
+        openNotificationReviewDialog();
+    }
+}
+
+function focusToCurrentIndex() {
+    if (currentIndex == 0) {
+        $("#back").css("border", "3px solid white").css("width", "calc(50% - 31px)").css("height", "34px");
+        $(".notification-item").css("border", "0").css("width", "calc(100% - 50px)");
+    } else {
+        $("#back").css("border", "0").css("width", "calc(50% - 25px)").css("height", "40px");
+        $(".notification-item").css("border", "0").css("width", "calc(100% - 50px)");
+        $(".notification-item:eq("+(currentIndex-1)+")").css("border", "3px solid white").css("width", "calc(100% - 56px)");
+    }
 }
--- php/get-notifications.php
+++ php/get-notifications.php
@@ -1,8 +1,8 @@
 <?php
 include 'db.php';
-$ip = $_SERVER["REMOTE_ADDR"];
+/*$ip = $_SERVER["REMOTE_ADDR"];
 $userId = $c->query("SELECT * FROM sessions WHERE ip='" . $ip . "'")->fetch_assoc()["user_id"];
-$c->query("UPDATE users SET last_update=" . round(microtime(true)*1000) . " WHERE id='" . $userId . "'");
+$c->query("UPDATE users SET last_update=" . round(microtime(true)*1000) . " WHERE id='" . $userId . "'");*/
 $results = $c->query("SELECT * FROM notifications");
 if ($results && $results->num_rows > 0) {
     $notifications = [];
