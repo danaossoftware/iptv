57d631c28a662bdf2230a9776a05325f84e0ed43
Dana Prakoso <danaossoftware@gmail.com>
2019-02-24 19:44:47 +0700

    commit

--- php/logout.php
+++ php/logout.php
@@ -4,4 +4,7 @@ include 'db.php';
 $c->query("DELETE FROM sessions WHERE ip='" . $ip . "'");*/
 session_id("jossstream");
 session_start();
+$userId = $_SESSION["jossstream_user_id"];
+$c->query("UPDATE users SET active_connections = active_connections - 1 WHERE id='" . $userId . "'");
 unset($_SESSION["jossstream_user_id"]);
+session_destroy();
