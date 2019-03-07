c3850e04af2bb84cb01f37e00e71bb16521ccbc1
Dana Prakoso <danaossoftware@gmail.com>
2019-03-07 09:13:23 +0700

    commit

--- php/add-user.php
+++ php/add-user.php
@@ -4,4 +4,4 @@ $userId = $_POST["user_id"];
 $email = $_POST["email"];
 $username = $_POST["username"];
 $password = $_POST["password"];
-$c->query("INSERT INTO users (id, email, password, username) VALUES ('" . $userId . "', '" . $email . "', '" . $password . "', '" . $username . "')");
+$c->query("INSERT INTO users (id, email, password, username, made_in) VALUES ('" . $userId . "', '" . $email . "', '" . $password . "', '" . $username . "', " . round(microtime(true)*1000) . ")");
