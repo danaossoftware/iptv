f871dc575f3f66fba630f1cf52d606c338fa8123
Dana Prakoso <danaossoftware@gmail.com>
2019-03-10 09:27:54 +0700

    commit

--- php/get-notifications.php
+++ php/get-notifications.php
@@ -3,7 +3,7 @@ include 'db.php';
 /*$ip = $_SERVER["REMOTE_ADDR"];
 $userId = $c->query("SELECT * FROM sessions WHERE ip='" . $ip . "'")->fetch_assoc()["user_id"];
 $c->query("UPDATE users SET last_update=" . round(microtime(true)*1000) . " WHERE id='" . $userId . "'");*/
-$results = $c->query("SELECT * FROM notifications DESC");
+$results = $c->query("SELECT * FROM notifications ORDER BY date DESC");
 if ($results && $results->num_rows > 0) {
     $notifications = [];
     while ($row = $results->fetch_assoc()) {
